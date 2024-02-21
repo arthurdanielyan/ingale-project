9b517ab6c878d12f307a1aad31b6a0395e5e2a8f
YervandAleksanyan <aleksanyan.e@office.lan>
2024-02-21 21:13:50 +0400

    started implementing

--- app/src/main/java/com/nightx/ingale/feature_local/main/presentation/LocalMainScreen.kt
+++ app/src/main/java/com/nightx/ingale/feature_local/main/presentation/LocalMainScreen.kt
@@ -77,24 +77,9 @@ fun LocalMainScreen(
         }
     }
 
-    var selectedTabIndex by rememberSaveable {
-        mutableIntStateOf(0)
-    }
     val pagerState = rememberPagerState(
         pageCount = { state.sections.size }
     )
-    val bottomBarController = LocalBottomBarController.current
-    LaunchedEffect(selectedTabIndex) {
-        bottomBarController.sendEffect(BottomBarEffect.ExpandMusicInfo)
-        pagerState.animateScrollToPage(
-            page = selectedTabIndex
-        )
-    }
-    LaunchedEffect(pagerState.currentPage, pagerState.isScrollInProgress) {
-        if (!pagerState.isScrollInProgress) {
-            selectedTabIndex = pagerState.currentPage
-        }
-    }
     Column(
         modifier = Modifier
             .background(
@@ -166,7 +151,7 @@ fun LocalMainScreen(
         Spacer(modifier = Modifier.height(16.dp))
         TabRow(
             modifier = Modifier.fillMaxWidth(),
-            selectedItemIndex = { selectedTabIndex },
+            pagerState = pagerState,
             items = state.sections,
             itemContent = { title ->
                 Text(
@@ -174,10 +159,7 @@ fun LocalMainScreen(
                     color = MaterialTheme.colorScheme.onBackground,
                     modifier = Modifier.padding(MaterialTheme.spacing.large)
                 )
-            },
-            onSelect = { index, _ ->
-                selectedTabIndex = index
-            },
+            }
         )
         HorizontalPager(
             state = pagerState,
--- app/src/main/java/com/nightx/ingale/feature_local/main/presentation/ui_components/TabRow.kt
+++ app/src/main/java/com/nightx/ingale/feature_local/main/presentation/ui_components/TabRow.kt
@@ -1,25 +1,31 @@
-package com.nightx.ingale.feature_local.main.presentation.ui_components
+package com.night.ingale.feature_local.main.presentation.ui_components
 
+import android.util.Log
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.animateIntAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.pager.PagerState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -29,20 +35,27 @@ import androidx.compose.ui.layout.positionInParent
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
-import com.nightx.ingale.mvi.wrappers.StableList
+import com.night.ingale.core.presentation.view.LaunchedEffect
+import com.night.ingale.mvi.wrappers.StableList
+import kotlinx.coroutines.launch
 import kotlin.math.roundToInt
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun <T> TabRow(
     modifier: Modifier = Modifier,
+    pagerState: PagerState,
     itemContent: @Composable (item: T) -> Unit,
-    onSelect: (index: Int, item: T) -> Unit,
-    selectedItemIndex: () -> Int,
     items: StableList<T>
 ) {
-    var selectedItemIndexUpt by remember(selectedItemIndex()) { mutableIntStateOf(selectedItemIndex()) }
+    var selectedItemIndex by remember {
+        mutableIntStateOf(pagerState.currentPage)
+    }
     var currentWidth by rememberSaveable {
         mutableFloatStateOf(0f)
+    }
+    val offsets = remember {
+        Array(items.size) { 0 }
     }
     var offset by rememberSaveable {
         mutableIntStateOf(0)
@@ -63,50 +76,87 @@ fun <T> TabRow(
         ),
         label = "offset of the line"
     )
+
+    var dragOffset by remember {
+        mutableIntStateOf(0)
+    }
+    var isTargetingInProgress by remember {
+        mutableStateOf(false)
+    }
+
+    LaunchedEffect(
+        pagerState.currentPage,
+        pagerState.isScrollInProgress,
+        pagerState.targetPage,
+        isTargetingInProgress,
+        pagerState.currentPageOffsetFraction
+    ) { currentPage, isScrollInProgress, targetPage, isTargetingInProgress, currentPageOffsetFraction ->
+        if (isTargetingInProgress) { // scrolling by click
+            selectedItemIndex = targetPage
+        } else if (isScrollInProgress) { // userDragging
+            selectedItemIndex = currentPage
+            val estimatedTargetPage =
+                (currentPage + if (currentPageOffsetFraction > 0) 1 else -1).coerceIn(
+                    0,
+                    offsets.size - 1
+                )
+            Log.d("myLogs", "fraction: $currentPageOffsetFraction")
+            dragOffset =
+                offsets[currentPage] + (currentPageOffsetFraction * offsets[estimatedTargetPage]).roundToInt()
+        }
+    }
+
+    val scope = rememberCoroutineScope()
     val density = LocalDensity.current
 
     Box(
-        contentAlignment = Alignment.BottomStart
+        modifier = modifier,
+        contentAlignment = Alignment.Center
     ) {
-        LazyRow(
-            modifier = modifier,
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            itemsIndexed(
-                items = items,
-                key = { index, _ ->
-                    index
-                }
-            ) { index, item ->
-                Box(
-                    modifier = Modifier
-                        .clickable {
-                            onSelect(index, item)
-                            selectedItemIndexUpt = index
-                        }
-                        .onGloballyPositioned {
-                            if (index == selectedItemIndexUpt) {
-                                currentWidth = density.run {
-                                    it.size.width.toDp().value
+        Box {
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                items.forEachIndexed { index, item ->
+                    Box(
+                        modifier = Modifier
+                            .clickable {
+                                scope.launch {
+                                    isTargetingInProgress = true
+                                    pagerState.animateScrollToPage(index)
+                                    isTargetingInProgress = false
                                 }
-
-                                offset = it.positionInParent().x.roundToInt()
+                                selectedItemIndex = index
                             }
-                        }
-                ) {
-                    itemContent(item)
+                            .onGloballyPositioned {
+                                val currentOffset = it.positionInParent().x.roundToInt()
+                                offsets[index] = currentOffset
+                                if (index == selectedItemIndex) {
+                                    currentWidth = density.run {
+                                        it.size.width.toDp().value
+                                    }
+                                }
+                            }
+                    ) {
+                        itemContent(item)
+                    }
                 }
             }
+
+            Box(
+                modifier = Modifier
+                    .offset {
+                        IntOffset(
+                            if (isTargetingInProgress) offsetAnim else dragOffset,
+                            0
+                        )
+                    }
+                    .align(Alignment.BottomStart)
+                    .width(widthAnim)
+                    .height(1.dp)
+                    .background(MaterialTheme.colorScheme.onBackground)
+            )
         }
-    }
-    key(offsetAnim) {
-        Box(
-            modifier = Modifier
-                .offset { IntOffset(offsetAnim, 0) }
-                .width(widthAnim)
-                .height(1.dp)
-                .background(MaterialTheme.colorScheme.onBackground)
-        )
     }
 }
