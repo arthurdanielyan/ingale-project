b1247ca2620f101ee5efd2affd913c073ca2d448
arturdanielyan1 <arturdanielyan19@gmail.com>
2024-03-08 23:15:54 +0400

    tabrow working

--- app/src/main/java/com/nightx/ingale/feature_local/main/presentation/ui_components/TabRow.kt
+++ app/src/main/java/com/nightx/ingale/feature_local/main/presentation/ui_components/TabRow.kt
@@ -1,14 +1,10 @@
 package com.nightx.ingale.feature_local.main.presentation.ui_components
 
-import android.util.Log
 import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.animateIntAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.collectIsDraggedAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
@@ -24,6 +20,7 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -32,7 +29,6 @@ import androidx.compose.ui.layout.positionInParent
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
-import com.nightx.ingale.core.presentation.view.LaunchedEffect
 import com.nightx.ingale.mvi.wrappers.StableList
 import kotlinx.coroutines.launch
 import kotlin.math.absoluteValue
@@ -61,61 +57,36 @@ fun <T> TabRow(
             }
         )
     }
-    var dragOffset by remember {
-        mutableIntStateOf(0)
-    }
-    var dragWidth by remember {
+    var dragWidth by rememberSaveable {
         mutableFloatStateOf(0f)
     }
-    val isUserDragging by pagerState.interactionSource.collectIsDraggedAsState()
-    val widthAnim by animateDpAsState(
-        targetValue = dragWidth.dp,
-        animationSpec = tween(
-            durationMillis = pageSettleDuration,
-            easing = LinearEasing
-        ),
-        label = "length of the line"
-    )
-    val offsetAnim by animateIntAsState(
-        targetValue = dragOffset,
-        animationSpec = tween(
-            durationMillis = if(!isUserDragging) {
-                pageSettleDuration
-            } else 0,
-            easing = LinearEasing
-        ),
-        label = "offset of the line"
-    )
-    LaunchedEffect(
+    val dragOffset = rememberSaveable(
         pagerState.isScrollInProgress,
-        pagerState.currentPageOffsetFraction,
-    ) { isScrollInProgress, fraction ->
-        Log.d("myLogs", "$isUserDragging")
-        if (!isUserDragging) { // scrolling by click
-            selectedItemIndex = pagerState.targetPage
-            dragWidth = tabsBounds[selectedItemIndex].width
-            dragOffset = tabsBounds[selectedItemIndex].offset
-        } else if (isScrollInProgress) { // userDragging
-            if(fraction > 0) {
-                pagerState.currentPage + 1
-            } else {
-                pagerState.currentPage
-            }
-            if(fraction > 0) {
-                val currentBounds = tabsBounds[pagerState.currentPage]
-                val targetBounds = tabsBounds[pagerState.currentPage+1]
-                val diff = targetBounds.offset - currentBounds.offset
-                val progress = fraction
-                dragOffset = currentBounds.offset + (progress*diff).roundToInt()
-                dragWidth = currentBounds.width + progress*(targetBounds.width-currentBounds.width)
-            } else if(fraction < 0) {
-                val currentBounds = tabsBounds[pagerState.currentPage]
-                val targetBounds = tabsBounds[pagerState.currentPage-1]
-                val diff = currentBounds.offset - targetBounds.offset
-                val progress = 1f-fraction.absoluteValue
-                dragOffset = targetBounds.offset + (progress*diff).roundToInt()
-                dragWidth = currentBounds.width + progress*(targetBounds.width-currentBounds.width)
-            }
+        pagerState.currentPageOffsetFraction
+    ) {
+        val fraction = pagerState.currentPageOffsetFraction
+        if(fraction > 0) {
+            pagerState.currentPage + 1
+        } else {
+            pagerState.currentPage
+        }
+        if(fraction > 0) {
+            val currentBounds = tabsBounds[pagerState.currentPage]
+            val targetBounds = tabsBounds[pagerState.currentPage+1]
+            val diff = targetBounds.offset - currentBounds.offset
+            val progress = fraction
+            dragWidth = currentBounds.width + progress*(targetBounds.width-currentBounds.width)
+            currentBounds.offset + (progress*diff).roundToInt()
+        } else if(fraction < 0) {
+            val currentBounds = tabsBounds[pagerState.currentPage]
+            val targetBounds = tabsBounds[pagerState.currentPage-1]
+            val diff = currentBounds.offset - targetBounds.offset
+            val progress = 1f-fraction.absoluteValue
+            dragWidth = currentBounds.width + progress*(targetBounds.width-currentBounds.width)
+            targetBounds.offset + (progress*diff).roundToInt()
+        } else { // fraction == 0
+            dragWidth = tabsBounds[pagerState.currentPage].width
+            tabsBounds[pagerState.currentPage].offset
         }
     }
 
@@ -154,8 +125,8 @@ fun <T> TabRow(
                                         offset = currentOffset
                                     )
                                 }
-                                if(selectedItemIndex == index) {
-                                    dragOffset = tabsBounds[index].offset
+                                if (selectedItemIndex == index) {
+//                                    dragOffset = tabsBounds[index].offset
                                     dragWidth = tabsBounds[index].width
                                 }
                             }
@@ -169,12 +140,13 @@ fun <T> TabRow(
                 modifier = Modifier
                     .offset {
                         IntOffset(
-                            offsetAnim,
+                            dragOffset,
                             0
                         )
                     }
                     .align(Alignment.BottomStart)
-                    .width(widthAnim)
+//                    .width(dragWidth.dp)
+                    .width(40.dp)
                     .height(1.dp)
                     .background(MaterialTheme.colorScheme.onBackground)
             )
