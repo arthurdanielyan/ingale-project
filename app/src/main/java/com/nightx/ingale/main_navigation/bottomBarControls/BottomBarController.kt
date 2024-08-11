7ca12c4ffdb640383fc93e3b573908736662b4be
OkelloSam <samuelokello929@gmail.com>
2024-08-12 02:10:31 +0300

    fix: Trigger BottomBarEffect on Lifecycle ON_START to Resolve Visibility Issue
     This commit addresses a bug where the BottomBar on the main screen becomes invisible when performing a system back action before the navigation animation completes. The issue occurred because the LaunchedEffect in the SendBottomBarEffect function was not triggered during this transition, causing the BottomBar not to reappear as expected.
    
     The fix involves triggering the BottomBarEffect in the ON_START lifecycle event, ensuring proper initialization and handling of the effect when the screen becomes visible again. This approach leverages the lifecycle-aware behavior of the composable, ensuring that the BottomBar's visibility is correctly managed even during rapid back navigation.

--- app/src/main/java/com/nightx/ingale/main_navigation/bottomBarControls/BottomBarController.kt
+++ app/src/main/java/com/nightx/ingale/main_navigation/bottomBarControls/BottomBarController.kt
@@ -1,8 +1,11 @@
 package com.nightx.ingale.main_navigation.bottomBarControls
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.staticCompositionLocalOf
-import com.nightx.ingale.core.presentation.view.SingleLaunchedEffect
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import kotlinx.coroutines.flow.Flow
 
 interface BottomBarController {
@@ -27,8 +30,18 @@ fun SendBottomBarEffect(
     effect: BottomBarEffect
 ) {
     val bottomBarController = LocalBottomBarController.current
-    SingleLaunchedEffect {
-        bottomBarController.sendEffect(effect)
+    val lifeCycleOwner = LocalLifecycleOwner.current
+
+    DisposableEffect(lifeCycleOwner) {
+        val observer = LifecycleEventObserver { _, event ->
+            if(event == Lifecycle.Event.ON_START) {
+                bottomBarController.sendEffect(effect)
+            }
+        }
+        lifeCycleOwner.lifecycle.addObserver(observer)
+        onDispose {
+            lifeCycleOwner.lifecycle.removeObserver(observer)
+        }
     }
 }
 
