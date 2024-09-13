525deaf7805265b79f9c1e49c6070c766f4f1e79
ToniRajinCoding <tonisan1080p@gmail.com>
2024-09-13 11:03:56 +0700

    disable click through musicbar

--- bottomBar/impl/src/main/java/com/nightx/ingale/bottomBar/impl/BottomNavigation.kt
+++ bottomBar/impl/src/main/java/com/nightx/ingale/bottomBar/impl/BottomNavigation.kt
@@ -4,6 +4,7 @@ import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -181,7 +182,7 @@ fun BottomNavigation() {
             }
             MusicBar(
                 modifier = Modifier
-                    .align(Alignment.BottomCenter)
+                    .align(Alignment.BottomCenter).clickable(enabled = false) {  }
             )
         }
     }
