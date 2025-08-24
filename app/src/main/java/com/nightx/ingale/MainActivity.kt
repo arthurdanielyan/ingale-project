3eb33d61baef088f69a95ca3735bdac63ec45ad9
Artur Danielyan <arturdanielyan19@gmail.com>
2025-08-24 21:34:46 +0400

    fix navigation bar background

--- app/src/main/java/com/nightx/ingale/MainActivity.kt
+++ app/src/main/java/com/nightx/ingale/MainActivity.kt
@@ -5,19 +5,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
 import com.arkivanov.decompose.retainedComponent
 import com.nightx.ingale.core.decompose.AppComponentContext
 import com.nightx.ingale.core.decompose.AppRouterImpl
@@ -25,6 +28,7 @@ import com.nightx.ingale.core.decompose.DefaultAppComponentContext
 import com.nightx.ingale.root.api.root.RootComponent
 import com.nightx.ingale.root.impl.root.ui.RootScreen
 import org.koin.android.ext.android.inject
+import kotlin.math.absoluteValue
 
 class MainActivity : ComponentActivity() {
 
@@ -41,7 +45,6 @@ class MainActivity : ComponentActivity() {
             )
         }
         setContent {
-            window.navigationBarColor = MaterialTheme.colorScheme.surface.toArgb()
             IngaleTheme {
 
                 enableEdgeToEdge(
@@ -50,45 +53,39 @@ class MainActivity : ComponentActivity() {
                         MaterialTheme.colorScheme.surface.toArgb(),
                     )
                 )
-                // A surface container using the 'background' color from the theme
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    RootScreen(rootComponent)
+                    Box {
+                        RootScreen(rootComponent)
+                        NavigationBarOverlay()
+                    }
                 }
             }
         }
     }
 
     @Composable
-    private fun StatusBarProtection(
-        color: Color = MaterialTheme.colorScheme.surfaceContainer,
-        heightProvider: () -> Float = calculateGradientHeight(),
-    ) {
-
-        Canvas(Modifier.fillMaxSize()) {
-            val calculatedHeight = heightProvider()
-            val gradient = Brush.verticalGradient(
-                colors = listOf(
-                    color.copy(alpha = 1f),
-                    color.copy(alpha = .8f),
-                    Color.Transparent
-                ),
-                startY = 0f,
-                endY = calculatedHeight
-            )
-            drawRect(
-                brush = gradient,
-                size = Size(size.width, calculatedHeight),
-            )
-        }
+    private fun BoxScope.NavigationBarOverlay() {
+        Box(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .fillMaxWidth()
+                .requiredHeight(
+                    getNavigationBarHeight()
+                )
+                .background(MaterialTheme.colorScheme.background)
+        )
     }
 
     @Composable
-    fun calculateGradientHeight(): () -> Float {
-        val statusBars = WindowInsets.statusBars
-        val density = LocalDensity.current
-        return { statusBars.getTop(density).times(1.2f) }
+    private fun getNavigationBarHeight(): Dp {
+        val navigationBars = WindowInsets.navigationBars
+        return LocalDensity.current.run {
+            (navigationBars.getTop(this) - navigationBars.getBottom(this))
+                .absoluteValue.toDp()
+        }
     }
 }
